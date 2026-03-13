54676bce23a4a0535a63d654d0228e673f61dbfa
Nikhil <kujurnikhil0007@gmail.com>
2026-03-13 07:18:33 -0700

    one

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+let healthy = true
+
+
+app.get("/health", (req, res) => {
+    if (healthy) {
+        return res.status(200).json({message: "app is healthy"})
+    } else {
+        return res.status(500).json({message: "app is unhealthy"})
+    }
+})
+
+// endpoint to break the app
+app.get("/stop", (req, res) => {
+    healthy = false
+    return res.json({message: "application stopped"})
+})
+
+// endpoint to recover manually
+app.get("/start", (req, res) => {
+    healthy = true
+    return res.json({message: "application started"})
+})
+
+app.listen(PORT, () => {
+    console.log(`service is started on port ${PORT}`)
+})
